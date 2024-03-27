50e70583cb6887c6b030bfdd15229fbb92526de7
lbc19920615 <1585638808@qq.com>
2024-03-27 17:07:26 +0800

    update id

--- src/ele.js
+++ src/ele.js
@@ -6,8 +6,21 @@ import { styleMap } from 'lit/directives/style-map.js';
 import './ele/base.js';
 import './ele/style.css';
 
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
 window.ZID = function () {
-  return window.crypto.randomUUID().replaceAll('-', '_');
+  // return window.crypto.randomUUID().replaceAll('-', '_');
+  return makeid(20)
 };
 
 function createFlatten() {
